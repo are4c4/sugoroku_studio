eb71640e248913412f7af095cdb560e98a7adc35
Are <235915091+are4c4@users.noreply.github.com>
2026-09-04 19:38:08 +0900

    feat: track player route history

--- lib/domain/player.dart
+++ lib/domain/player.dart
@@ -7,6 +7,7 @@ class Player {
     required this.type,
     required this.currentSquareId,
     this.skipTurns = 0,
+    this.routeHistory = const <String>[],
   });
 
   final String id;
@@ -14,12 +15,14 @@ class Player {
   final PlayerType type;
   final String currentSquareId;
   final int skipTurns;
+  final List<String> routeHistory;
 
   Player copyWith({
     String? name,
     PlayerType? type,
     String? currentSquareId,
     int? skipTurns,
+    List<String>? routeHistory,
   }) {
     return Player(
       id: id,
@@ -27,6 +30,7 @@ class Player {
       type: type ?? this.type,
       currentSquareId: currentSquareId ?? this.currentSquareId,
       skipTurns: skipTurns ?? this.skipTurns,
+      routeHistory: routeHistory ?? this.routeHistory,
     );
   }
 }
